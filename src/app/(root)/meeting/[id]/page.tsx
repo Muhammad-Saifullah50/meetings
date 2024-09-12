10efd480606e0f72c60c679a65c32adf618d8397
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2024-09-12 15:34:46 +0500

    developing meeting room

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,6 +1,8 @@
 'use client'
+import Loader from '@/components/Loader';
 import MeetingRoom from '@/components/MeetingRoom';
 import MeetingSetup from '@/components/MeetingSetup';
+import { useGetCallById } from '@/hooks/useGetCallById';
 import { useUser } from '@clerk/nextjs'
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
 import { useState } from 'react';
@@ -11,12 +13,18 @@ const MeetingPage = ({ params }: { params: { id: string } }) => {
 
     const [isSetupComplete, setIsSetupComplete] = useState(false);
 
+    const { call, callLoading } = useGetCallById(params.id);
+
+    if (!isLoaded || callLoading) {
+        return <Loader />
+    }
+
     return (
         <main className='h-screen w-full'>
-            <StreamCall call={}>
+            <StreamCall call={call}>
                 <StreamTheme>
                     {!isSetupComplete ? (
-                        <MeetingSetup />
+                        <MeetingSetup setisSetupComplete={setIsSetupComplete} />
                     ) : (
                         <MeetingRoom />
                     )}
--- src/hooks/useGetCallById.ts
+++ src/hooks/useGetCallById.ts
@@ -1,10 +1,33 @@
-import { Call } from "@stream-io/video-react-sdk"
-import { useState } from "react"
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk"
+import { useEffect, useState } from "react"
 
 export const useGetCallById = (id: string | string[]) => {
 
   const [call, setCall] = useState<Call>();
+  const [callLoading, setCallLoading] = useState(true);
 
-  
-  return
+  const client = useStreamVideoClient();
+
+  useEffect(() => {
+    if (!client) return;
+
+    const loadCall = async () => {
+      const { calls } = await client.queryCalls({
+        filter_conditions: {
+          id
+        }
+      });
+
+      if (calls.length > 0) {
+        setCall(calls[0])
+      }
+
+      setCallLoading(false);
+    }
+
+    loadCall();
+  }, [client, id])
+
+
+  return { call, callLoading }
 }
